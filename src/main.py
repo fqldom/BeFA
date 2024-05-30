76d583cc535fb3aa4abab347d4ef077c6f6816a8
fqldom <fqltreap@outlook.com>
2024-05-30 20:05:32 +0800

    update

--- src/main.py
+++ src/main.py
@@ -17,7 +17,7 @@ os.environ['NUMEXPR_MAX_THREADS'] = '48'
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('--model', '-m', type=str, default='MINE', help='name of models')
+    parser.add_argument('--model', '-m', type=str, default='LATTICE_BeFA', help='name of models')
     parser.add_argument('--dataset', '-d', type=str, default='Microlens', help='name of datasets')
 
     config_dict = {
